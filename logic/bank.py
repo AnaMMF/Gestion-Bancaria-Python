b37477530f0bf9cd8bab6bba4290c925788c7351
AnaMMF <anamoyafdez@outlook.es>
2026-04-14 16:39:01 +0200

    mas logica Bank

--- logic/bank.py
+++ logic/bank.py
@@ -19,3 +19,33 @@ class Bank:
             if acc.id == id:
                 return acc
         return None
+
+    def list_accounts(self):
+        # Imprime la lista de cuentas registradas usando el método __str__ de la clase BankAccount
+        return [str(acc) for acc in self.accounts]
+
+    def deposit(self, id: str, amount: float) -> bool:
+        # Busca la cuenta por el DNI asociado y si la encuentra añade el importe al saldo, devuelve True si se ha realizado el ingreso o False si no se ha encontrado la cuenta
+        #TODO: Devolver un mensaje de error si el importe es negativo o cero y si la cuenta no se ha encontrado
+        if amount <= 0:
+            print("Invalid deposit amount. Please enter a positive value.")
+            return False
+
+        account = self.find_account(id)
+        if account:
+            account.amount += amount
+            return True
+        return False
+
+    def withdraw(self, id: str, amount: float) -> bool:
+        # Busca la cuenta por el DNI asociado y si la encuentra resta el importe al saldo, devuelve True si se ha realizado el retiro o False si no se ha encontrado la cuenta o si el saldo es insuficiente
+        #TODO: Devolver un mensaje de error si no se encuentramla cuenta
+
+        account = self.find_account(id)
+        if account:
+            if account.amount - amount >= 0:
+                account.amount -= amount
+                return True
+            else:
+                print("Withdrawal failed. Insufficient balance.")
+        return False
